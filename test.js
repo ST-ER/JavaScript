b5f90803a7f604e0093ab725bcfde6af89dbc3bf
STE <egorstarovojtov001@gmail.com>
2023-01-24 11:13:44 +0300

    Lesson1

---
+++ test.js
@@ -0,0 +1,7 @@
+//"use strict"; //Включает строгий режим ядра
+let string = "foo";
+const array = ["f", "o", "o"];
+var number = 10; //Переменная VAR уже не используется из-за области вид..
+
+console.log(number);
+let str = "hello";
